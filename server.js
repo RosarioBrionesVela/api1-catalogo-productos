e2ec8ce142903a9246cc542fdbc5f313d8562dae
CHAYITO <st4442@utr.edu.mx>
2026-06-14 14:56:52 -0600

    Sprint 1 - Conexion MongoDB y modelo Producto

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 
 const app = require("./src/app");
+const conectarDB = require("./src/config/db");
+
+conectarDB();
 
 const PORT = process.env.PORT || 3000;
 
---
+++ src/models/Producto.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const productoSchema = new mongoose.Schema(
+{
+    nombre: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: String,
+        required: true
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    descripcion: {
+        type: String
+    }
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model("Producto", productoSchema);
